30f9218973b06883a1116a9bb571ef3188977322
Jorge Alberto Iglesias <33730243+iglesias9627@users.noreply.github.com>
2024-03-02 11:38:39 +0100

    Add Rackup gem and test sinatra framework

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source "https://rubygems.org"
 
 # gem "rails"
 gem "sinatra"
+gem "rackup", "~> 2.1"
--- app.rb
+++ app.rb
@@ -1,1 +1,14 @@
-puts 'Hello world'
+# puts 'Hello world'
+# app.rb
+
+require 'sinatra'
+require 'json'
+
+puts "My application is running in: http://localhost:4567/"
+
+get '/' do
+  # here we specify the content type to respond with
+  content_type :json
+
+  { message: "Hello world!"}.to_json
+end
