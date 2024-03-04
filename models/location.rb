27010593ed82dbbb42e06fa6ffab2c42e54330e9
Jorge Alberto Iglesias <33730243+iglesias9627@users.noreply.github.com>
2024-03-04 13:56:09 +0100

    Models for locations and temperatures

---
+++ models/location.rb
@@ -0,0 +1,17 @@
+require 'mongoid'
+require_relative './temperature.rb'
+
+class Location
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :latitude, type: Float
+  field :longitude, type: Float
+  field :slug, type: String
+
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+  validates :slug, presence: true, uniqueness: true, format: { with: /\A[a-z0-9_-]+\z/i }
+
+  has_many :temperatures
+end
