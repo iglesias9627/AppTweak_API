d151da64a5c8a33df43f42d5842fe83f7407c9d7
Jorge Alberto Iglesias <33730243+iglesias9627@users.noreply.github.com>
2024-03-04 20:52:43 +0100

    Retrieve data from database or from API/save

--- api/routes/temperature.rb
+++ api/routes/temperature.rb
@@ -41,13 +41,41 @@ module Routes
           return { error: 'Date range is more than 3 days' }.to_json
         end
 
+        list_dates_filtered = []
+
         location = LocationModel.find_by(slug: slug)
 
-        list_dates = api_7timer_query(location, start_date, end_date)
+        # Check if data already exists in the database for the specified date range
+        existing_temperatures = location.temperature_models.where(date: { '$gte' => start_date, '$lte' => end_date })
 
-        list_dates_filtered = filter_by_dates(start_date, end_date, list_dates)
+        if existing_temperatures.any?
+          # Data exists in the database, retrieve and return it
+          list_dates_filtered = existing_temperatures.map do |temperature|
+            {
+              'date' => temperature.date,
+              'min-forecasted' => temperature.min_forecasted,
+              'max-forecasted' => temperature.max_forecasted
+            }
+          end
+          puts list_dates_filtered
+        else
+          # Data doesn't exists in database so we retreive data from 7Timer API and we save in database
+          list_dates = api_7_timer_query(location, start_date, end_date)
+
+          list_dates.each do |temp_data|
+            temperature = location.temperature_models.find_or_initialize_by(date: temp_data['date'])
+            temperature.min_forecasted = temp_data['min-forecasted']
+            temperature.max_forecasted = temp_data['max-forecasted']
+            unless temperature.save
+              status 400
+              return {error: "Error saving record: #{temperature.errors.full_messages.join(', ')}"}.to_json
+            end
+          end
+          list_dates_filtered = filter_by_dates(start_date, end_date, list_dates)
+        end
 
         { response: list_dates_filtered  }.to_json
+
       rescue  Mongoid::Errors::DocumentNotFound => e
         status 404
         { error: 'Location not found' }.to_json
